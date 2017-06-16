dd0b48f2f7abaf5509538ca334f0600f1153f380
Junseo Youn <plancatlog@gmail.com>
2017-06-16 23:26:34 +0900

    post 작성

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,5 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'is_public', 'created_at']
+    list_display = ['id', 'title', 'created_at']
     list_display_links = ['title']
-    list_editable = ['is_public']
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def index(request):
     post_list = Post.objects.all()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+    else:
+        form = PostForm()
+
     return render(request, 'blog/post_list.html', {
         'post_list' : post_list,
+        'form' : form
     })
 
 def post_detail(request, pk):
